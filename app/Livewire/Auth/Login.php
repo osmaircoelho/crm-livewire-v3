98f1366dc0e5608bbbeb236f6d330461992dd581
Osmair Coelho <osmair.coelho@gmail.com>
2024-07-10 15:45:23 -0300

    crm-2: be able to successfully log in

---
+++ app/Livewire/Auth/Login.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Login extends Component
+{
+    public ?string $email = null;
+
+    public ?string $password = null;
+
+    public function render(): View
+    {
+        return view('livewire.auth.login');
+    }
+
+    public function tryToLogin(): void
+    {
+        if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            return;
+        }
+
+        $this->redirect(route('dashboard'));
+    }
+
+}
---
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Livewire\Auth\Login;
+use App\Models\User;
+use Livewire\Livewire;
+
+it('renders successfully', function () {
+    Livewire::test(Login::class)
+        ->assertOk();
+});
+
+it('should be able to login', function () {
+    $user = User::factory()->create([
+        'email'    => 'joe@joe.com',
+        'password' => 'password',
+    ]);
+
+    Livewire::test(Login::class)
+        ->set('email', 'joe@joe.com')
+        ->set('password', 'password')
+        ->call('tryToLogin')
+        ->assertHasNoErrors()
+        ->assertRedirect(route('dashboard'));
+
+    expect(auth()->check())->toBeTrue()
+        ->and(auth()->user())->id->toBe($user->id);
+});
