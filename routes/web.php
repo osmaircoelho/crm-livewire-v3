64bdcaee0284a647a06e1294c997b3804608e086
Osmair Coelho <osmair.coelho@gmail.com>
2024-07-18 11:36:18 -0300

    crm-4: needs to have a password recovery route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => auth()->logout())->name('auth.logout');
+Route::get('/password/recovery', fn () => 'oi')->name('auth.password.recovery');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
