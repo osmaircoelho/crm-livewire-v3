8bb1d99cd3b80cd2c4deba9308522ce6b00064f4
osmair.coelho@gmail.com <osmair.coelho@gmail.com>
2024-08-17 17:37:58 -0300

    crm-4: entering into the password reset page checking valid and invalid tokens

---
+++ tests/Feature/Auth/Password/ResetTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Livewire\Auth\Password;
+use App\Models\User;
+use Illuminate\Auth\Notifications\ResetPassword;
+
+use function Pest\Laravel\get;
+
+test('need to receive a valid token with a combination with the email', function () {
+    Notification::fake();
+
+    $user = User::factory()->create();
+
+    Livewire::test(Password\Recovery::class)
+        ->set('email', $user->email)
+        ->call('startPasswordRecovery');
+
+    Notification::assertSentTo(
+        $user,
+        ResetPassword::class,
+        function (ResetPassword $notification) {
+            get(route('password.reset') . '?token=' . $notification->token)
+                ->assertSuccessful();
+
+            get(route('password.reset') . '?token=any-token')
+                ->assertRedirect(route('login'));
+
+            return true;
+        }
+    );
+
+});
