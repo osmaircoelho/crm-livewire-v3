d264b5d19e76f2dc3307c10d188f3724bb00f258
Osmair Coelho <osmair.coelho@gmail.com>
2024-08-21 18:31:43 -0300

    crm-4: making sure that password is being updated

--- tests/Feature/Auth/Password/ResetTest.php
+++ tests/Feature/Auth/Password/ResetTest.php
@@ -5,8 +5,10 @@ use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 
 use function Pest\Laravel\get;
+use function PHPUnit\Framework\assertTrue;
 
-test('need to receive a valid token with a combination with the email', function () {
+test('need to receive a valid token with a combination with the email and open the page', function () {
+
     Notification::fake();
 
     $user = User::factory()->create();
@@ -29,4 +31,37 @@ test('need to receive a valid token with a combination with the email', function
         }
     );
 
+});
+
+test('test if is possible to reset the password with the given token', function () {
+    Notification::fake();
+
+    $user = User::factory()->create();
+
+    Livewire::test(Password\Recovery::class)
+        ->set('email', $user->email)
+        ->call('startPasswordRecovery');
+
+    Notification::assertSentTo(
+        $user,
+        ResetPassword::class,
+        function (ResetPassword $notification) use ($user) {
+            Livewire::test(
+                Password\Reset::class,
+                ['token' => $notification->token, 'email' => $user->email]
+            )
+                ->set('email_confirmation', $user->email)
+                ->set('password', 'new-password')
+                ->set('password_confirmation', 'new-password')
+                ->call('updatePassword')
+                ->assertHasNoErrors()
+                ->assertRedirect(route('dashboard'));
+
+            $user->refresh();
+
+            assertTrue(Hash::check('new-password', $user->password));
+
+            return true;
+        }
+    );
 });
