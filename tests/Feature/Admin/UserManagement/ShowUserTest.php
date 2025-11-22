c4b35d3d51ace6563554deeaa2282bcdb35bbc10
Osmair Coelho <osmair.coelho@gmail.com>
2025-11-22 16:01:53 -0300

    crm-23: display user information in the screen :: without layout

---
+++ tests/Feature/Admin/UserManagement/ShowUserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Livewire\Admin;
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+
+beforeEach(function () {
+    // You can set up any necessary preconditions here
+    $this->admin       = User::factory()->admin()->create();
+    $this->userDeleted = User::factory()->deleted()->create();
+
+    actingAs($this->admin);
+
+});
+
+it('should be able to show all the details of the user in the component', function () {
+
+    $userDeleted   = $this->userDeleted;
+    $userDeletedId = $this->userDeleted->id;
+
+    Livewire::test(Admin\Users\Show::class)
+        ->call('loadUser', $userDeletedId)
+        // propriedades do componente show
+        ->assertSet('user.id', $userDeletedId)
+        ->assertSet('modal', true)
+
+        ->assertSee($userDeleted->name)
+        ->assertSee($userDeleted->email)
+
+        ->assertSee($userDeleted->created_at->format('d/m/Y H:i'))
+        ->assertSee($userDeleted->updated_at->format('d/m/Y H:i'))
+        ->assertSee($userDeleted->deleted_at->format('d/m/Y H:i'))
+        ->assertSee($userDeleted->deletedBy->name);
+
+});
