058858fbe5066cbf8f8891e27363b45d6e1a45cf
Osmair Coelho <osmair.coelho@gmail.com>
2025-11-26 12:49:17 -0300

    crm-8: adding a key impersonate to the session  with the given user

---
+++ tests/Feature/Admin/UserManagement/ImpersonateTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Livewire\Admin\Users\Impersonate;
+use App\Models\User;
+
+use function PHPUnit\Framework\{assertSame, assertTrue};
+
+it('should add a key impersonate to the session  with the given user', function () {
+
+    $user = User::factory()->create();
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $user->id);
+
+    assertTrue(session()->has('impersonate'));
+
+    assertSame(session()->get('impersonate'), $user->id);
+
+});
